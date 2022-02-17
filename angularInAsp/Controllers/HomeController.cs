5d9dbb8e95026c1418efc588df039115b967a143
mhdalkhbaz <mohamadalkhbaz98@gmail.com>
2022-02-17 05:37:24 +0200

    Add project files.

---
+++ angularInAsp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using angularInAsp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace angularInAsp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CompanyDBEntities db = new CompanyDBEntities();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Employees
+        public JsonResult GetEmployeesJson()
+        {
+                var emp = db.Employees.ToList();
+
+            return new JsonResult { Data = emp, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+         }
+
+
+
+        public JsonResult GetEmployee(int id)
+        {
+            var emp = db.Employees.Find(id);
+            
+            return new JsonResult { Data = emp, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+
+
+        [HttpPost]
+        public ActionResult SaveRecord([Bind(Include = "Id,Name,Department,Salary")] Employee employee)
+        {
+            
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                 return RedirectToAction("About");
+
+        }
+      
+
+    
+    }
+}
